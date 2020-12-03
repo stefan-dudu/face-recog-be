317139826d72e4291bbec67551865eb8c2c387c4
stefan01-dev <stefan01.dev@gmail.com>
2020-12-03 06:14:27 +0200

    working on DB but an error occured

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')
+
+knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'Stefan',
+    password : '',
+    database : 'smart-brain'
+  }
+});
 
 const app = express();
 app.use(bodyParser.json());
