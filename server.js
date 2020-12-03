800ccefbc4192cccb76e42cb7560b982ee9814b8
stefan01-dev <stefan01.dev@gmail.com>
2020-12-03 06:20:55 +0200

    fixed cors & knex modules not found

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex')
+
+knex({
+  client: 'pg',
+  connection: {
+    host : '127.0.0.1',
+    user : 'Stefan',
+    password : '',
+    database : 'smart-brain'
+  }
+});
 
 const app = express();
 app.use(bodyParser.json());
@@ -41,13 +52,13 @@ app.get('/', (req, res) => {
 
 app.post('/signin', (req, res)=>{
 
-    // Load hash from your password DB.
-    bcrypt.compare("bacon", '$2a$10$xAiGiB2zJvYEZnaCTVJBz.gE8np8lnbDjKFqPXxOsVMfMzSDosO0W', function(err, res) {
-        console.log('first try', res)
-    });
-    bcrypt.compare("apples", '$2a$10$xAiGiB2zJvYEZnaCTVJBz.gE8np8lnbDjKFqPXxOsVMfMzSDosO0W', function(err, res) {
-        console.log('2nd try', res)
-    });
+    // // Load hash from your password DB.
+    // bcrypt.compare("bacon", '$2a$10$xAiGiB2zJvYEZnaCTVJBz.gE8np8lnbDjKFqPXxOsVMfMzSDosO0W', function(err, res) {
+    //     console.log('first try', res)
+    // });
+    // bcrypt.compare("apples", '$2a$10$xAiGiB2zJvYEZnaCTVJBz.gE8np8lnbDjKFqPXxOsVMfMzSDosO0W', function(err, res) {
+    //     console.log('2nd try', res)
+    // });
 
     if(req.body.email === database.users[0].email &&
        req.body.password === database.users[0].password) {
