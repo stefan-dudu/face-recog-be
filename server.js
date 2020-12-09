1684abe24841aa0c2bad072443c6e340a8ca44a3
stefan01-dev <stefan01.dev@gmail.com>
2020-12-09 06:04:38 +0200

    -all good so far

--- server.js
+++ server.js
@@ -56,27 +56,52 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res)=>{
-    if(req.body.email === database.users[0].email &&
-       req.body.password === database.users[0].password) {
-           res.json('username and password are OK')
-       } else {
-           res.status(400).json('there is a problem')
-       }
+    db.select('email', 'hash').from('login')
+        .where('email', '=', req.body.email)
+        .then(data => {     
+            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+            if(isValid) {
+               return db.select('*').from('users')
+                    .where('email', '=', req.body.email)
+                    .then(user => {
+                        res.json(user[0])
+                    })
+                    .catch(err => res.status(400).json('cannot get user'))
+            }
+            else{
+            res.status(400).json('wrong credentials')}
+        })
+        .catch(err => res.status(400).json('wrong credentials'))
 });
 
 app.post('/register', (req, res)=>{
     const{email, name, password} = req.body;
-    db('users')
-        .returning('*')
-        .insert({
-            email: email,
-            name: name,
-            joined: new Date()
-    })
-        .then(user => {
-            res.json(user[0])
-    }) // in case of an error, we catch it in this way
-        .catch(err => res.status(400).json('unable to join'))
+    const hash = bcrypt.hashSync(password);
+        db.transaction(trx => {
+            trx.insert({
+                hash: hash,
+                email: email,
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0],
+                        name: name,
+                        joined: new Date()
+                    })
+                    .then(user => {
+                        res.json(user[0])
+                    }) 
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+        })
+
+    // in case of an error, we catch it in this way
+        .catch(err => res.status(400).json('unable to register'))
     
 })
 
