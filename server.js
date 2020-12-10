a6e28e7e18251e66287faf62ec9870bd12291efa
stefan01-dev <stefan01.dev@gmail.com>
2020-12-10 05:33:32 +0200

    got rid of the old db

--- server.js
+++ server.js
@@ -23,36 +23,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const database = {
-    users:[
-        {
-            id:'123',
-            name:'John',
-            email: 'john@gmail.com',
-            password: 'cookies',
-            entries: 0,
-            joined: new Date()
-        },
-                {
-            id:'124',
-            name:'Sally',
-            email: 'sally@gmail.com',
-            password: 'kahoot',
-            entries: 0,
-            joined: new Date()
-        }
-    ],
-    login:[
-        {
-            id:'987',
-            has: '',
-            email: 'john@gmail.com'
-        }
-    ]
-}
-
 app.get('/', (req, res) => {
-    res.send(database.users);
+    res.send('success');
 })
 
 app.post('/signin', (req, res)=>{
