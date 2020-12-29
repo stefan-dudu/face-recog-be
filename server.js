890e072244a3c73075e62df71a9e1e8f7a365a7c
stefan01-dev <stefan01.dev@gmail.com>
2020-12-29 06:35:39 +0200

    mod the host and ssl

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-adjacent-53806',
-    user : 'Stefan',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
