646f582fbbbb5a9e695967e2f4c75ebfce030277
stefan01-dev <stefan01.dev@gmail.com>
2020-12-30 06:34:28 +0200

    API key hidden

--- controllers/image.js
+++ controllers/image.js
@@ -2,7 +2,7 @@ const Clarifai = require('clarifai');
 
 //You must add your own API key here from Clarifai. 
 const app = new Clarifai.App({
- apiKey: '1ec4031a38cb43d1bb99d5eed1740991'
+ apiKey: process.env.API
 });
 
 const handleApiCall = (req, res) => {
